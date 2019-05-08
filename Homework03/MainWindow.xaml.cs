cfc836cccc7341236090105e0e6aa1f13c2c183b
Muhammad Z. Khan <mzkhan2@uw.edu>
2019-05-08 02:01:33 -0400

    .cs file for HW03. The code is tested. Please provide me feedback if it needs to improve or if a bug is noticed

---
+++ Homework03/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+//Name: Muhammad Khan
+//Homework 03
+//Date: 05/02/2019
+
+
+namespace Homework03
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var users = new List<Models.User>();
+
+            users.Add(new Models.User { Name = "Dave", Password = "1DavePwd" });
+            users.Add(new Models.User { Name = "Steve", Password = "2StevePwd" });
+            users.Add(new Models.User { Name = "Lisa", Password = "3LisaPwd" });
+            uxList.ItemsSource = users;
+        }
+
+        private void uxListColumnHeader_Click(object sender, RoutedEventArgs e)
+        {
+            GridViewColumnHeader columnHeader = (sender as GridViewColumnHeader);
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(uxList.ItemsSource);
+            string columnSort = columnHeader.Tag.ToString();
+            uxList.Items.SortDescriptions.Clear();
+            ListSortDirection direction = ListSortDirection.Ascending;
+           uxList.Items.SortDescriptions.Add(new SortDescription(columnSort, direction));
+        }
+    }
+}
