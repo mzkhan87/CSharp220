f2fb0c5fca751d393c6111a8db73aa563bf266ae
Muhammad Z. Khan <mzkhan2@uw.edu>
2019-05-30 19:21:43 -0400

    Homwork 6

---
+++ Homework06/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Homework06.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult RsvpForm(Models.SendCard sendCard)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View("MissingFields", sendCard);
+            }
+            else
+            {
+                return View("AllValidFields",sendCard);
+            }
+            
+        }
+
+    }
+}
---
+++ Homework06/Models/SendCard.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Homework06.Models
+{
+    public class SendCard
+    {
+        [Required]
+        public string From { get; set; }
+        [Required]
+        public string To { get; set; }
+        [Required]
+        public string Message { get; set; }
+    }
+
+
+
+}
